9a0e8a1053a2ff54e025bf294a764bb9e4929cd6
d5ng <dong5ffice@gmail.com>
2024-11-06 20:48:35 +0900

    ✨ Feat: register 함수 추가

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,5 +1,15 @@
-import { useState } from "react"
-import type { FieldValues, UseFormParams, UseFormReturn, UseFormState } from "@/types/form"
+import { ChangeEventHandler, FocusEventHandler, useCallback, useEffect, useRef, useState } from "react"
+import type {
+  FieldElement,
+  FieldState,
+  FieldValues,
+  RegisterOptions,
+  UseFormParams,
+  UseFormRegister,
+  UseFormReturn,
+  UseFormState,
+} from "@/types/form"
+import { validateField } from "@/utils/validation"
 
 export default function useForm<TFieldValues extends FieldValues = FieldValues>({
   defaultValues,
@@ -11,7 +21,72 @@ export default function useForm<TFieldValues extends FieldValues = FieldValues>(
     isSubmitting: false,
   })
 
+  const validateOptions = useRef<Partial<Record<keyof TFieldValues, RegisterOptions>>>({})
+
+  const handleChange: ChangeEventHandler<FieldElement> = useCallback(
+    (event) =>
+      setFormState((prevFormState) => ({
+        ...prevFormState,
+        values: { ...prevFormState.values, [event.target.name]: event.target.value },
+      })),
+    []
+  )
+
+  const handleBlur: FocusEventHandler<FieldElement> = useCallback((event) => {
+    setFormState((prevFormState) => ({
+      ...prevFormState,
+      touchedFields: { ...prevFormState.touchedFields, [event.target.name]: true },
+    }))
+  }, [])
+
+  const handleError = useCallback(
+    (error: FieldState<TFieldValues, string>) =>
+      setFormState((prevFormState) => ({
+        ...prevFormState,
+        errors: { ...prevFormState.errors, ...error },
+      })),
+    []
+  )
+
+  const validateForm = useCallback((values: TFieldValues) => {
+    let errors: FieldState<TFieldValues, string> = {}
+
+    for (const field in values) {
+      const fieldErrors = validateField(
+        values[field],
+        field as keyof TFieldValues,
+        validateOptions.current[field] as RegisterOptions
+      )
+
+      errors = { ...errors, ...fieldErrors }
+    }
+
+    return errors
+  }, [])
+
+  useEffect(() => {
+    const errors = validateForm(formState.values)
+    handleError(errors)
+  }, [formState.values, handleError, validateForm])
+
+  const register: UseFormRegister<TFieldValues> = useCallback(
+    (name, options) => {
+      if (options) {
+        validateOptions.current[name] = options
+      }
+
+      return {
+        name,
+        value: formState.values[name],
+        onChange: handleChange,
+        onBlur: handleBlur,
+      }
+    },
+    [formState.values, handleBlur, handleChange]
+  )
+
   return {
     formState,
+    register,
   }
 }
--- src/types/form.ts
+++ src/types/form.ts
@@ -1,7 +1,13 @@
+import { ChangeEventHandler, FocusEventHandler } from "react"
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type FieldValues = Record<any, string>
+export type FieldValues = Record<string, any>
 
 export type FieldState<TFieldValues, FieldStateType> = Partial<Record<keyof TFieldValues, FieldStateType>>
+
+export type FieldElement = HTMLInputElement | HTMLTextAreaElement
+
+export type FieldName<TFieldValues extends FieldValues> = keyof TFieldValues
 
 export interface UseFormState<TFieldValues> {
   values: TFieldValues
@@ -14,6 +20,26 @@ export interface UseFormParams<TFieldValues> {
   defaultValues: TFieldValues
 }
 
-export interface UseFormReturn<TFieldValues> {
+export interface UseFormReturn<TFieldValues extends FieldValues> {
   formState: UseFormState<TFieldValues>
+  register: UseFormRegister<TFieldValues>
+}
+
+export type RegisterOptions = Partial<{
+  required: { value: boolean; message: string }
+  min: { value: number; message: string }
+  max: { value: number; message: string }
+  pattern: { value: RegExp; message: string }
+}>
+
+export type UseFormRegister<TFieldValues extends FieldValues> = <TFieldName extends keyof TFieldValues>(
+  name: TFieldName,
+  options?: RegisterOptions
+) => UseFormRegisterReturn<TFieldName, TFieldValues>
+
+export type UseFormRegisterReturn<TFieldName extends keyof TFieldValues, TFieldValues> = {
+  name: TFieldName
+  onChange: ChangeEventHandler<FieldElement>
+  onBlur: FocusEventHandler<FieldElement>
+  value: TFieldValues[TFieldName]
 }
